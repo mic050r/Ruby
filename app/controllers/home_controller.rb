9333479f2452f9fe2d546fe921333351792d4c62
2214_임지현 <s2216@e-mirim.hs.kr>
2024-03-10 11:17:37 +0000

    ✨ : 글 작성하면 db에 저장되도록 추

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,21 @@ class HomeController < ApplicationController
 	def index
 		
 	end
+	
+# 	글쓰기 폼
+	def write
+		
+	end
+	
+# 	글쓰기 액션
+	def create 
+		# 정보를 저장하는 코드
+		post = Post.new
+		post.title = params[:title]
+		post.content = params[:content]
+		post.save
+		
+		redirect_to '/index'
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 	# /index 로 접속하면 home controller 안에 들어있는 index를 실행하라
 	get '/index' => 'home#index'
+	get '/write' => 'home#write'
+	post '/create' => 'home#create'
+	
 	get '/add' => 'calculator#add'
 	post '/result' => 'calculator#result'
 	
